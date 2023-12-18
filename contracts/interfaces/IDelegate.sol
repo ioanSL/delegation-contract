ff0fa7e9b438065ed9eae9a5c64d56b4ec712f5a
ioanSL <io@shardlabs.io>
2023-12-18 20:17:34 +0200

    feat: v2 with ERC20 and ERC1155 support

--- contracts/interfaces/IDelegate.sol
+++ contracts/interfaces/IDelegate.sol
@@ -7,39 +7,80 @@ interface IDelegate {
         ACTIVE
     }
 
-    event Delegated(
+    // EVENTS
+    event DelegatedERC721(
         address indexed delegator,
         address indexed receiver,
         address indexed asset,
-        uint256 tokenId
+        uint256 tokenId,
+        bool enable
     );
 
-    event Undelegated(
+    event DelegatedERC20(
         address indexed delegator,
         address indexed receiver,
         address indexed asset,
-        uint256 tokenId
+        uint256 value,
+        bool enable
     );
 
-    function delegate(
+    event DelegatedERC1155(
+        address indexed delegator,
+        address indexed receiver,
+        address indexed asset,
+        uint256 tokenId,
+        uint256 value,
+        bool enable
+    );
+
+    // FUNCTIONS
+    function delegateERC721(
         address receiver,
         address asset,
         uint256 tokenId,
-        bytes32 rights
+        bytes32 rights,
+        bool enable
     ) external;
 
-    function undelegate(
+    function delegateERC1155(
         address receiver,
         address asset,
         uint256 tokenId,
-        bytes32 rights
+        uint256 amount,
+        bytes32 rights,
+        bool enable
     ) external;
 
-    function checkDelegate(
+    function delegateERC20(
+        address receiver,
+        address asset,
+        uint256 amount,
+        bytes32 rights,
+        bool enable
+    ) external;
+
+    function checkDelegateERC721(
         address delegator,
         address receiver,
         address asset,
         uint256 tokenId,
+        bytes32 rights
+    ) external view returns (bool);
+
+    function checkDelegateERC1155(
+        address delegator,
+        address receiver,
+        address asset,
+        uint256 tokenId,
+        uint256 amount,
+        bytes32 rights
+    ) external view returns (bool);
+
+    function checkDelegateERC20(
+        address delegator,
+        address receiver,
+        address asset,
+        uint256 amount,
         bytes32 rights
     ) external view returns (bool);
 }
