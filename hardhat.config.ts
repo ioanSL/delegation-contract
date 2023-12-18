73cd6d8981865a46014fed29af9d573e0e20de4d
ioanSL <io@shardlabs.io>
2023-12-18 13:51:26 +0200

    feat: gas report with gas price setup

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,17 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-gas-reporter"
+
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
 
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
+  gasReporter: {
+    enabled: (process.env.REPORT_GAS) ? true : false,
+    currency: 'USD',
+    coinmarketcap: COINMARKETCAP_API_KEY,
+    token: "ETH"
+  }
 };
 
 export default config;
