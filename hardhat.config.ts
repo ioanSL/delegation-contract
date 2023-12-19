e7e10d13ccca4830fc7126cd3f2f03b7f1c7c103
ioanSL <io@shardlabs.io>
2023-12-19 10:29:46 +0200

    feat: connection to sepolia network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,6 +5,14 @@ import "hardhat-gas-reporter"
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
 
 const config: HardhatUserConfig = {
+  networks: {
+    hardhat: {
+    },
+    sepolia: {
+      url: process.env.ALCHEMY_SEPOLIA_RPC || "",
+      accounts: [process.env.PRIVATE_KEY || ""]
+    },
+  },
   solidity: {
     version: "0.8.20", 
     settings: {
